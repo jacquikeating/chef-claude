3459ae557b6990e77d73a2feb21a74837f32913e
jacquikeating <j.keating@live.ca>
2025-07-09 22:49:28 -0800

    Basic add ingredient functionality
    - Imported useState and declared the ingredients state variable (an array).
    - Added a .map() function inside the <ul> to render all items in the ingredients array as <li>s.
    - Made addIngredient() function for form submissions. The string entered in the input field is added to the ingredients array.
    - Gave the <form> action={addIngredient}.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Main = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
   return (
     <main>
-      <form>
+      <form action={addIngredient}>
         <input
           type="text"
           placeholder="e.g. oregano"
@@ -11,7 +20,11 @@ const Main = () => {
         <button>Add ingredient</button>
       </form>
 
-      <ul></ul>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key="ingredient">{ingredient}</li>
+        ))}
+      </ul>
     </main>
   );
 };
