bf92b0ef9c7c939aeacd8cc013944f3f3b9dae67
jacquikeating <j.keating@live.ca>
2025-07-10 23:49:27 -0400

    Wrote further conditionals
    - Wrote another conditional to only render the call to action <div> when at least 4 items are added to ingredients.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -28,13 +28,16 @@ const Main = () => {
               <li key="ingredient">{ingredient}</li>
             ))}
           </ul>
-          <div className="get-recipe-container">
-            <div>
-              <h3>Ready for a recipe?</h3>
-              <p>Generate a recipe from your list of ingredients.</p>
+
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
             </div>
-            <button>Get a recipe</button>
-          </div>
+          )}
         </section>
       )}
     </main>
